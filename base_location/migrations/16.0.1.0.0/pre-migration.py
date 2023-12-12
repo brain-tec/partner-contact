4d1ead943098a566ea15466ded1e39f688f20046
BT-cmorales <carlos.morales@braintec.com>
2023-12-12 08:42:56 +0100

    Fix pre-migration

--- base_location/migrations/16.0.1.0.0/pre-migration.py
+++ base_location/migrations/16.0.1.0.0/pre-migration.py
@@ -14,21 +14,9 @@ def migrate(env, version):
         }
     )
     openupgrade.remove_tables_fks(env.cr, ['res_better_zip'])
-    
-    cr.execute(
-        """UPDATE res_partner rp
-        SET city_id = rcz.city_id
-        FROM res_city_zip rcz
-        WHERE rp.city_id IS NULL AND rp.zip_id = rcz.id"""
-    )
-    
-    # From V12 to V13
 
-    # Make city information consistent, as in previous versions, this field was
-    # conditionally filled depending if the country has enforced such data, and
-    # now, it's always filled.
-    
-    cr.execute(
+    # From V12 to V13
+    env.execute(
         """UPDATE res_partner rp
         SET city_id = rcz.city_id
         FROM res_city_zip rcz
