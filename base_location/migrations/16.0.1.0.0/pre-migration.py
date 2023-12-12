6056e926d65e648342b57143da85db881036337d
BT-cmorales <carlos.morales@braintec.com>
2023-12-12 08:57:19 +0100

    Fix cr on pre-migration script

--- base_location/migrations/16.0.1.0.0/pre-migration.py
+++ base_location/migrations/16.0.1.0.0/pre-migration.py
@@ -16,7 +16,7 @@ def migrate(env, version):
     openupgrade.remove_tables_fks(env.cr, ['res_better_zip'])
 
     # From V12 to V13
-    env.execute(
+    env.cr.execute(
         """UPDATE res_partner rp
         SET city_id = rcz.city_id
         FROM res_city_zip rcz
